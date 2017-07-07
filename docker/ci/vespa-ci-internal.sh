2eb6f87dc4ce72a5bf15cdca14aaf37b0af59727
gjoranv <gv@yahoo-inc.com>
2017-07-07 14:53:12 +0200

    Add docker file structure.

---
+++ docker/ci/vespa-ci-internal.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+set -e
+set -x
+
+if [ "$(uname)" != "Darwin" ]; then
+    free -m
+fi
+
+df -h .
+
+NUM_CORES=$(nproc --all)
+
+#SOURCE_DIR=~/java-design-patterns
+#mkdir "${SOURCE_DIR}"
+#git clone --no-hardlinks /java-design-patterns "${SOURCE_DIR}"
+
+#cd "${SOURCE_DIR}"
+
+whoami
+cd java-design-patterns
+pwd
+ls -l
+
+time mvn clean install -U -V -e -DskipTests=true -Dmaven.javadoc.skip=true
+
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+time mvn install -T 2.0C -nsu -q -V -e -Dmaven.test.redirectTestOutputToFile=true
+
+#MAVEN_OPTS="-Xms128m -Xmx512m" mvn install -Dmaven.test.redirectTestOutputToFile=true
+
+echo "Success!"
